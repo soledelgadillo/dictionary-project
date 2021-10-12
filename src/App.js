d96164a66fc944b05d2bb36d55f6865a68de190b
Sol Delgadillo <soledelgadillo@gmail.com>
2021-10-12 17:16:46 +0100

    Added Search Engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import Dictionary from "./Dictionary";
 import logo from "./logo.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          This app was coded by Sol Delgadillo, and it's{" "}
+          <a href="https://github.com/soledelgadillo/dictionary-project">
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
